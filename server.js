f385f52f18f90f3413a6022f0cedb9dd628c9108
Harper Foley <harper.e.foley@gmail.com>
2022-11-16 05:46:55 +0000

    Adds constructor and returns queried data to user

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.get('/weather', (request, response) => {
   let lat = request.query.lat;
   let lon = request.query.lon;
   let searchQuery = weatherData.filter(city => (city.lat === lat && city.lon === lon));
-  
-  searchQuery.length < 1 ? response.status(500).send('Error. City not covered by system.') : response.status(200).send(searchQuery);
+  let threeDayForecast = new Forecast(searchQuery);
+  threeDayForecast.length < 1 ? response.status(500).send('Error. City not covered by system.') : response.status(200).send(threeDayForecast);
 });
 
 // this will run for any route not defined above
@@ -35,3 +35,23 @@ app.get('*', (request, response) => {
 
 // LISTEN
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+// CLASSES
+
+class Forecast {
+  constructor(weatherObj) {
+    this.day1 = {
+      date: weatherObj[0].data[0].datetime,
+      description: `Low of ${weatherObj[0].data[0].min_temp}, high of ${weatherObj[0].data[0].max_temp} with ${weatherObj[0].data[0].weather.description.toLowerCase()}`
+    };
+    this.day2 = {
+      date:  weatherObj[0].data[1].datetime,
+      description: `Low of ${weatherObj[0].data[1].min_temp}, high of ${weatherObj[0].data[1].max_temp} with ${weatherObj[0].data[1].weather.description.toLowerCase()}`
+    };
+    this.day3 = {
+      date:  weatherObj[0].data[2].datetime,
+      description: `Low of ${weatherObj[0].data[2].min_temp}, high of ${weatherObj[0].data[2].max_temp} with ${weatherObj[0].data[2].weather.description.toLowerCase()}`
+    };
+  };
+};
