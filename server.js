3ea483d460dbdb3b73e0eb20f8d4fce8852da7c4
Harper Foley <harper.e.foley@gmail.com>
2022-11-17 01:03:21 +0000

    Adds movie API and constructor

--- server.js
+++ server.js
@@ -20,12 +20,20 @@ app.use(cors());
 app.get('/weather', async (request, response) => {
   let lat = request.query.lat;
   let lon = request.query.lon;
-  console.log(request);
   let weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=3&key=${process.env.WEATHER_API_KEY}`;
   let searchQuery = await axios.get(weatherURL);
   let threeDayForecast = searchQuery.data.data.map(day => new Forecast(day));
   threeDayForecast.length < 1 ? response.status(500).send('Error. City not covered by system.') : response.status(200).send(threeDayForecast);
 });
+
+app.get('/movie', async (request, response) => {
+  let searchTerm = request.query.search;
+  let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchTerm}`;
+  let movieResults = await axios.get(movieURL);
+  let topMovies = movieResults.data.results.map(movie => new Movie(movie));
+  topMovies.length < 1 ? response.status(500).send('Error. Movie not found.') : response.status(200).send(topMovies);
+})
+
 
 app.get('/', (request, response) => {
   response.send('Hello, from our server');
@@ -52,5 +60,16 @@ class Forecast {
     this.low = weatherObj.low_temp;
     this.high = weatherObj.max_temp;
     this.fullDescription = `Low of ${this.low}, high of ${this.high} with ${this.description}.`;
+  };
+};
+
+class Movie {
+  constructor(movieObj) {
+    this.title = movieObj.title;
+    this.overview = movieObj.overview;
+    this.averageRating = movieObj.vote_average;
+    this.totalReviews = movieObj.vote_count;
+    this.imgPath = movieObj.poster_path;
+    this.releaseDate = movieObj.release_date;
   };
 };
